3afe2a583bcdc4f47528ef0fb05f050f87b9eef9
Leandro <leandro.andrioli@outlook.com>
2019-09-10 08:23:26 +0100

    Triple comparison exercise

---
+++ HackerRankChallenges/HackerRankChallenges/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace HackerRankChallenges
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+            List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+
+            List<int> result = compareTriplets(a, b);
+
+            textWriter.WriteLine(String.Join(" ", result));
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+
+        static List<int> compareTriplets(List<int> a, List<int> b)
+        {
+            var result = new List<int>();
+            int alicePoints = 0;
+            int bobPoints = 0;
+
+            for (int i = 0; i < a.Count; i++)
+            {
+                if (a[i] > b[i])
+                    alicePoints++;
+                else if (a[i] < b[i])
+                    bobPoints++;
+            }
+
+            result.Add(alicePoints);
+            result.Add(bobPoints);
+
+            return result;
+        }
+    }
+}
---
+++ HackerRankChallenges/HackerRankChallenges/WarmUp/CompareTheTriplets.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRankChallenges.WarmUp
+{
+    //https://www.hackerrank.com/challenges/compare-the-triplets/problem
+    public class CompareTheTriplets
+    {
+        // Complete the compareTriplets function below.
+        static List<int> compareTriplets(List<int> a, List<int> b)
+        {
+            var result = new List<int>();
+            int alicePoints = 0;
+            int bobPoints = 0;
+
+            for (int i = 0; i < a.Count; i++)
+            {
+                if (a[i] > b[i])
+                    alicePoints++;
+                else if (a[i] < b[i])
+                    bobPoints++;
+            }
+
+            result.Add(alicePoints);
+            result.Add(bobPoints);
+
+            return result;
+        }
+
+        static List<int> compareTripletsTwo(List<int> a, List<int> b)
+        {
+            var result = new List<int>();
+
+            int alicePoints = ((a[0] > b[0]) ? 1 : 0) + ((a[1] > b[1]) ? 1 : 0) + ((a[2] > b[2]) ? 1 : 0);
+            int bobPoints = ((a[0] < b[0]) ? 1 : 0) + ((a[1] < b[1]) ? 1 : 0) + ((a[2] < b[2]) ? 1 : 0);
+
+            result.Add(alicePoints);
+            result.Add(bobPoints);
+
+            return result;
+        }
+    }
+}
