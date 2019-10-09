e579ca19979d11dc2739bdbe687684bd40242601
Leandro <leandro.andrioli@outlook.com>
2019-10-09 09:11:48 +0100

    Hashtables challenge ranson notes ongoing

--- HackerRankChallenges/HackerRankChallenges/Program.cs
+++ HackerRankChallenges/HackerRankChallenges/Program.cs
@@ -8,9 +8,42 @@ namespace HackerRankChallenges
 {
     public class Program
     {
-        
+        // First Attemps -> Your code did not execute within the time limits
+        static void checkMagazine2(string[] magazine, string[] note)
+        {
+            for (int i = 0; i < note.Length; i++)
+            {
+                if (!magazine.Contains(note[i]))
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+                magazine[i] = string.Empty;
+            }
+            Console.WriteLine("Yes");
+        }
+        static void checkMagazine(string[] magazine, string[] note)
+        {
+            HashSet<string> hash = new HashSet<string>(magazine);
+
+            for (int i = 0; i < note.Length; i++)
+            {
+                if (!hash.Contains(note[i]))
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+                hash.Remove(note[i]);
+            }
+            Console.WriteLine("Yes");
+        }
+
         static void Main(string[] args)
         {
+            checkMagazine(new string[] { "casa" }, new string[] { "Casa" });
+            checkMagazine(new string[] { "casa teste" }, new string[] { "casa" });
+            checkMagazine(new string[] { "two", "times", "three", "is", "not", "four" }, new string[] { "two", "times", "two", "is", "four" });
+            
         }
     }
 }
