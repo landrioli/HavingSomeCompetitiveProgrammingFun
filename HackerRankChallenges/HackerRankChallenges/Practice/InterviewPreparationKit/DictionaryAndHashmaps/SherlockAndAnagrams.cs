3e09a984658a7e2227831aca4d9c7d9eb52876b5
Leandro Andrioli <leandro.andrioli@outlook.com>
2019-10-14 13:57:51 +0100

    Sherlock And Anagrams Challenge

---
+++ HackerRankChallenges/HackerRankChallenges/Practice/InterviewPreparationKit/DictionaryAndHashmaps/SherlockAndAnagrams.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    public class SherlockAndAnagrams
+    {
+        static int SherlockAndAnagramsMethod(string s)
+        {
+            var dic = new Dictionary<string, int>();
+            var count = 0;
+            var substrings = GetSubstrings(s);
+
+            foreach (var substring in substrings)
+            {
+                Console.WriteLine(substring);
+
+                if (dic.ContainsKey(substring))
+                {
+                    var value = dic[substring];
+                    count += value;
+
+                    dic[substring] = value + 1;
+                }
+                else
+                {
+                    dic.Add(substring, 1);
+                }
+            }
+
+            return count;
+        }
+
+        static IEnumerable<string> GetSubstrings(string s)
+        {
+            var listPairs = new List<string>();
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                for (var j = 1; j <= s.Length - i; j++)
+                {
+                    var substring = s.Substring(i, j);
+                    var chars = substring.ToCharArray();
+                    // The jump of the gat - order so you do not need to deal with format each key after to comparison
+                    Array.Sort(chars);
+
+                    listPairs.Add(new string(chars));
+                }
+            }
+            return listPairs;
+        }
+    }
+}
