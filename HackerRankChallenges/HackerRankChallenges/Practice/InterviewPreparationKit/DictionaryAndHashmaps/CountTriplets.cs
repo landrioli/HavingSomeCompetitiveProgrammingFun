e15d38def295aed6bdbc8c4fdfe0f49a87ea20f4
Leandro Andrioli <leandro.andrioli@outlook.com>
2019-10-15 12:00:59 +0100

    Count triples challenge

---
+++ HackerRankChallenges/HackerRankChallenges/Practice/InterviewPreparationKit/DictionaryAndHashmaps/CountTriplets.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    class CountTriplets
+    {
+        //https://www.hackerrank.com/challenges/count-triplets-1
+        static long countTriplets(List<long> arr, long r)
+        {
+            var mp2 = new System.Collections.Generic.Dictionary<long, long>();
+            var mp3 = new System.Collections.Generic.Dictionary<long, long>();
+            long res = 0;
+            foreach (long val in arr)
+            {
+                if (mp3.ContainsKey(val))
+                    res += mp3[val];
+                if (mp2.ContainsKey(val))
+                    if (mp3.ContainsKey(val * r))
+                        mp3[val * r] += mp2[val];
+                    else
+                        mp3[val * r] = mp2[val];
+                if (mp2.ContainsKey(val * r))
+                    mp2[val * r]++;
+                else
+                    mp2[val * r] = 1;
+            }
+            return res;
+        }
+    }
+}
