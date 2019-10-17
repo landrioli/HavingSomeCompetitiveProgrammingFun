7e3dc9cf68d73d9c58d5a5388c0672a6b77729ea
Leandro <leandro.andrioli@outlook.com>
2019-10-17 18:58:00 +0100

    changes

---
+++ HackerRankChallenges/HackerRankChallenges/Practice/InterviewPreparationKit/Sorting/MarkAndToys.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRankChallenges.Practice.InterviewPreparationKit.Sorting
+{
+    //https://www.hackerrank.com/challenges/mark-and-toys/problem
+    class MarkAndToys
+    {
+        // Complete the maximumToys function below.
+        static int maximumToys(int[] prices, int k)
+        {
+            int maximumToys = 0;
+            long totalSpent = 0;
+            var orderedPrices = prices.OrderBy(p => p);
+
+            foreach (var price in orderedPrices)
+            {
+                totalSpent += price;
+                if (totalSpent > k)
+                {
+                    break;
+                }
+                maximumToys++;
+            }
+
+            return maximumToys;
+        }
+    }
+}
--- HackerRankChallenges/HackerRankChallenges/Program.cs
+++ HackerRankChallenges/HackerRankChallenges/Program.cs
@@ -9,61 +9,10 @@ namespace HackerRankChallenges
 {
     public class Program
     {
-        // Complete the twoStrings function below.
-        static string twoStrings(string s1, string s2)
-        {
-            var s1HashSet = new HashSet<char>(s1);
-            var s2HashSet = new HashSet<char>(s2);
-
-            foreach (var item in s1HashSet)
-            {
-                if (s2HashSet.Contains(item))
-                    return "YES";
-            }
-            return "NO";
-        }
-
-        // Complete the twoStrings function below.
-        static string twoStringsChar(string s1, string s2)
-        {
-            var s1HashSet = s1.ToCharArray();
-            var s2HashSet = s2.ToCharArray();
-
-            foreach (var item in s1HashSet)
-            {
-                if (s2HashSet.Contains(item))
-                    return "YES";
-            }
-            return "NO";
-        }
-
 
         static void Main(string[] args)
         {
-            var a = new StringBuilder();
-            for (int i = 0; i < 100000; i++)
-            {
-                a.Append("a");
-            }
-            a.Append("b");
-            var b = new StringBuilder();
-            for (int i = 0; i < 100000; i++)
-            {
-                b.Append("b");
-            }
-
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-            Console.WriteLine(twoStrings(a.ToString(), b.ToString()));
-            stopwatch.Stop();
-            Console.WriteLine("TIME WITH HASSET: {0}", stopwatch.Elapsed);
-
-            Stopwatch stopwatch2 = new Stopwatch();
-            stopwatch2.Start();
-            Console.WriteLine(twoStringsChar(a.ToString(), b.ToString()));
-
-            stopwatch2.Stop();
-            Console.WriteLine("TIME WITH CHAR ARRAY: {0}", stopwatch2.Elapsed);
         }
+        
     }
 }
