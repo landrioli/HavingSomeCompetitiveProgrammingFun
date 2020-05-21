3368bfe4ad9348e53eb659771cbc75c704466fbe
Leandro Andrioli <landrioli@eservicios.indracompany.com>
2020-05-21 09:46:14 +0100

    Challenges - :D

---
+++ LeetCodeChallenges/30DaysChallengeCovid/DaySixGroupAnagrams.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeChallenges._30DaysChallengeCovid
+{
+    public class DaySixGroupAnagrams
+    {
+        //Given an array of strings, group anagrams together.
+        //Example:
+        //Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+        //Output:
+        //[
+        //  ["ate","eat","tea"],
+        //  ["nat","tan"],
+        //  ["bat"]
+        //]
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var result = new List<IList<string>>();
+            if (strs.Length == 0)
+                return result;
+
+            var dic = new Dictionary<string, List<string>>();
+            foreach (var str in strs)
+            {
+                var strArray = str.ToCharArray();
+                Array.Sort(strArray);
+                var key = new string(strArray);
+                if (!dic.ContainsKey(key))
+                    dic.Add(key, new List<string>());
+                dic[key].Add(str);
+            }
+
+            foreach (var item in dic)
+            {
+                result.Add(item.Value);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCodeChallenges/Arrays/FindFirstAndLastOcurrencyUsingBinarySearch.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeChallenges.Arrays
+{
+    //https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+    /*
+        Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+        Your algorithm's runtime complexity must be in the order of O(log n).
+        If the target is not found in the array, return [-1, -1].
+            Example 1:
+            Input: nums = [5,7,7,8,8,10], target = 8
+            Output: [3,4]
+            Example 2:
+            Input: nums = [5,7,7,8,8,10], target = 6
+            Output: [-1,-1]
+         */
+    public class FindFirstAndLastOcurrencyUsingBinarySearch
+    {
+        //First, because we are locating the leftmost (or rightmost) index containing target (rather than returning true iff we find target), the algorithm does not terminate as soon as we find a match. 
+        //Instead, we continue to search until lo == hi and they contain some index at which target can be found.
+        public int[] SearchRangeWithBinarySearch(int[] nums, int target)
+        {
+            int[] result = new int[2];
+            result[0] = FIndBegin(nums, target);
+            result[1] = FindLast(nums, target);
+            return result;
+        }
+        //5, 7, 7, 8, 8, 8, 10 
+        private int FIndBegin(int[] nums, int target)
+        {
+            int idx = -1;
+            int start = 0;
+            int end = nums.Length - 1;
+            while (start <= end)
+            {
+                int mid = (start + end) / 2;
+                if (nums[mid] >= target)
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+                if (nums[mid] == target) idx = mid;
+            }
+            return idx;
+        }
+
+        private int FindLast(int[] nums, int target)
+        {
+            int idx = -1;
+            int start = 0;
+            int end = nums.Length - 1;
+            while (start <= end)
+            {
+                int mid = (start + end) / 2;
+                if (nums[mid] <= target)
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
+                if (nums[mid] == target) idx = mid;
+            }
+            return idx;
+        }
+    }
+}
