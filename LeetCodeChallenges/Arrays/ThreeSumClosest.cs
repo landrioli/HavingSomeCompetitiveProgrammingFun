cc107d6fbf27a809cdde0dd896401a3def3cdecc
Leandro <leandro.andrioli@outlook.com>
2020-05-29 10:58:41 +0100

    Training

---
+++ LeetCodeChallenges/Arrays/ThreeSumClosest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeChallenges.Arrays
+{
+    public class ThreeSumClosestPack
+    {
+        public int ThreeSumClosest(int[] nums, int target)
+        {
+            int closestSum = int.MaxValue;
+            int selectedVal = 0;
+            var hash = new HashSet<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    for (int k = j + 1; k < nums.Length; k++)
+                    {
+                        int sum = nums[i] + nums[j] + nums[k];
+                        if (Math.Abs(Math.Abs(target) - Math.Abs(sum)) < Math.Abs(closestSum))
+                            closestSum = sum;
+                    }   
+                }
+            }
+            // Given array nums = [-1, 2, -4, 1], and target = 1.
+            // The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+
+            return closestSum;
+        }
+    }
+}
---
+++ LeetCodeChallenges/WeeklyContest/WeeklyContest189.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeChallenges.WeeklyContest
+{
+    public class WeeklyContest189
+    {
+        public int BusyStudent(int[] startTime, int[] endTime, int queryTime)
+        {
+            int totalStudents = 0;
+            for (int i = 0; i < startTime.Length; i++)
+            {
+                if (startTime[i] <= queryTime && endTime[i] <= queryTime)
+                    totalStudents++;
+            }
+
+            return totalStudents;
+        }
+
+        public string ArrangeWords(string text)
+        {
+            var words = text.Split(' ');
+            var dict = new Dictionary<int, List<string>>();
+            foreach (var word in words)
+            {
+                if (dict.ContainsKey(word.Length))
+                {
+                    dict[word.Length].Add(word);
+                }
+                else
+                {
+                    dict.Add(word.Length, new List<string> { word });
+                }
+            }
+            var result = new StringBuilder();
+            foreach (var size in dict.Keys.OrderBy(p => p))
+            {
+                var wordsPerSize = dict[size];
+                wordsPerSize.ForEach(p => result.Append(p.ToLower() + " "));
+            }
+
+            var final = result.ToString();
+            return char.ToUpper(final[0]) + final.Substring(1, result.Length - 2);
+        }
+
+        public IList<int> PeopleIndexes(List<List<string>> favoriteCompanies)
+        {
+            // TODO: Continue here
+            //var listHashes = new List<HashSet<string>>();
+            //foreach (var favoriteSet in favoriteCompanies)
+            //{
+            //    listHashes.Add(new HashSet<string>(favoriteSet));
+            //}
+
+            //var result = new List<int>();
+            //for (int i = 0; i < favoriteCompanies.Count; i++)
+            //{
+            //    var isSubset = false;
+            //    for (int j = 0; j < listHashes[j].Count; j++)
+            //    {
+            //        listHashes[j].All(p=>p.Contains())
+            //    }
+            //}
+
+            //return result;
+            return null;
+        }
+
+
+        public void Main()
+        {
+            //BusyStudent(new int[] { 1, 2, 3 }, new int[] { 3, 2, 7 }, 4);
+
+            var final = new List<List<string>>
+            {
+                new List<string> { "a","b","c","d","e","f" },
+                new List<string> { "a","b","d","e","f" },
+                new List<string> { "b", "c", "d", "e", "f" }
+            };
+
+            PeopleIndexes(final);
+        }
+    }
+}
