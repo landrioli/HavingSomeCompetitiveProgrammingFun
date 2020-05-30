75add2972a6d58e20453c487067a463a916a73a6
Leandro Andrioli <landrioli@eservicios.indracompany.com>
2020-05-30 13:45:18 +0100

    Finishing last question from context 189

--- LeetCodeChallenges/WeeklyContest/WeeklyContest189.cs
+++ LeetCodeChallenges/WeeklyContest/WeeklyContest189.cs
@@ -8,6 +8,11 @@ namespace LeetCodeChallenges.WeeklyContest
 {
     public class WeeklyContest189
     {
+        //https://leetcode.com/contest/weekly-contest-189/problems/number-of-students-doing-homework-at-a-given-time/
+        //Given two integer arrays startTime and endTime and given an integer queryTime.
+        //The ith student started doing their homework at the time startTime[i] and finished it at time endTime[i].
+        //Return the number of students doing their homework at time queryTime.More formally, return the number of students where queryTime lays
+        //in the interval[startTime[i], endTime[i]] inclusive.
         public int BusyStudent(int[] startTime, int[] endTime, int queryTime)
         {
             int totalStudents = 0;
@@ -20,65 +25,64 @@ namespace LeetCodeChallenges.WeeklyContest
             return totalStudents;
         }
 
+        //https://leetcode.com/problems/rearrange-words-in-a-sentence/
+        //Given a sentence text(A sentence is a string of space-separated words) in the following format:
+        //First letter is in upper case.
+        //Each word in text are separated by a single space.
+        //    Your task is to rearrange the words in text such that all words are rearranged in an increasing order of their lengths.If two words have the same length, arrange them in their original order.
+        //    Return the new text following the format shown above.
         public string ArrangeWords(string text)
         {
-            var words = text.Split(' ');
-            var dict = new Dictionary<int, List<string>>();
-            foreach (var word in words)
-            {
-                if (dict.ContainsKey(word.Length))
-                {
-                    dict[word.Length].Add(word);
-                }
-                else
-                {
-                    dict.Add(word.Length, new List<string> { word });
-                }
-            }
-            var result = new StringBuilder();
-            foreach (var size in dict.Keys.OrderBy(p => p))
-            {
-                var wordsPerSize = dict[size];
-                wordsPerSize.ForEach(p => result.Append(p.ToLower() + " "));
-            }
-
-            var final = result.ToString();
-            return char.ToUpper(final[0]) + final.Substring(1, result.Length - 2);
+            var words = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            words = words.OrderBy(x => x.Length).Select(x => x.ToLowerInvariant()).ToArray();
+            words[0] = (words[0].Substring(0, 1).ToUpperInvariant() + words[0].Substring(1));
+            var finalString = string.Join(" ", words);
+            return finalString;
         }
 
+        ////https://leetcode.com/contest/weekly-contest-189/problems/people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list/
+        //People Whose List of Favorite Companies Is Not a Subset of Another List
+        //    Given the array favoriteCompanies where favoriteCompanies[i] is the list of favorites companies for the ith person(indexed from 0).
+        //Return the indices of people whose list of favorite companies is not a subset of any other list of favorites companies.You must return the indices in increasing order.
         public IList<int> PeopleIndexes(List<List<string>> favoriteCompanies)
         {
-            // TODO: Continue here
-            //var listHashes = new List<HashSet<string>>();
-            //foreach (var favoriteSet in favoriteCompanies)
-            //{
-            //    listHashes.Add(new HashSet<string>(favoriteSet));
-            //}
+            var ctoi = new Dictionary<string, List<int>>();
+            for (var i = 0; i < favoriteCompanies.Count; ++i)
+            {
+                foreach (var c in favoriteCompanies[i])
+                {
+                    if (!ctoi.ContainsKey(c)) ctoi[c] = new List<int>();
+                    ctoi[c].Add(i);
+                }
+            }
+            var result = new List<int>();
+            var cur = new HashSet<int>();
+            for (var i = 0; i < favoriteCompanies.Count; ++i)
+            {
+                cur.UnionWith(ctoi[favoriteCompanies[i][0]]);
+                for (var j = 1; j < favoriteCompanies[i].Count; ++j)
+                {
+                    var next = ctoi[favoriteCompanies[i][j]];
+                    cur.IntersectWith(next);
+                    if (cur.Count == 1) break;
+                }
+                if (cur.Count == 1)
+                    result.Add(cur.First());
+                cur.Clear();
+            }
 
-            //var result = new List<int>();
-            //for (int i = 0; i < favoriteCompanies.Count; i++)
-            //{
-            //    var isSubset = false;
-            //    for (int j = 0; j < listHashes[j].Count; j++)
-            //    {
-            //        listHashes[j].All(p=>p.Contains())
-            //    }
-            //}
-
-            //return result;
-            return null;
+            return result;
         }
 
 
         public void Main()
         {
             //BusyStudent(new int[] { 1, 2, 3 }, new int[] { 3, 2, 7 }, 4);
-
             var final = new List<List<string>>
             {
-                new List<string> { "a","b","c","d","e","f" },
-                new List<string> { "a","b","d","e","f" },
-                new List<string> { "b", "c", "d", "e", "f" }
+                new List<string> { "a","b","c","d" },
+                new List<string> { "a","d" },
+                new List<string> { "e", "f" }
             };
 
             PeopleIndexes(final);
