2d28e8e4cea0d26a14d61c75c91b3dbf770a1c8c
EX100019 <landrioli@eservicios.indracompany.com>
2020-07-08 14:02:23 +0100

    Challenges amz

---
+++ LeetCodeChallenges/AMZ/04FourSearchSuggestionsSystem.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeChallenges.AMZ
+{
+    public class FourSearchSuggestionsSystem
+    {
+        //Slow Solution - Brute Force
+        public IList<IList<string>> SuggestedProductsBruteForce(string[] products, string searchWord)
+        {
+            products = products.OrderBy(p => p).ToArray();
+            var finalList = new List<IList<string>>();
+            var letters = searchWord.ToCharArray();
+            var currentWord = string.Empty;
+
+            for (int i = 0; i < letters.Length; i++)
+            {
+                var currentlist = new List<string>();
+                currentWord += letters[i];
+                for (int j = 0; j < products.Length; j++)
+                {
+                    if (products[j].StartsWith(currentWord))
+                    {
+                        currentlist.Add(products[j]);
+                    }
+
+                    if (currentlist.Count == 3 || j + 1 == products.Length)
+                    {
+                        finalList.Add(currentlist);
+                        break;
+                    }
+                }
+            }
+            return finalList;
+        }
+
+        /*As the products arrays will be ordered, we can binary search to find the first ocurrency and after that we try to get the index plus 2 + words that match
+ O(n logn)*/
+        public IList<IList<String>> SuggestedProductsImplementingBinarySearch(String[] products, String searchWord)
+        {
+            products = products.OrderBy(p => p).ToArray();
+            IList<IList<String>> ans = new List<IList<string>>();
+            for (int i = 1; i <= searchWord.Length; i++)
+            {
+                int index = BinarySearch(products, searchWord, i);
+                if (index == -1)
+                {
+                    ans.Add(new List<string>());
+                    continue;
+                }
+                List<String> prefixes = new List<string>();
+                for (int j = 0; j < 3 && index + j < products.Length; j++)
+                {
+                    if (Compare(products[j + index], searchWord, i) == 0)
+                    {
+                        prefixes.Add(products[j + index]);
+                    }
+                }
+                ans.Add(prefixes);
+            }
+            return ans;
+        }
+
+        private int BinarySearch(String[] p, String s, int len)
+        {
+            int l = 0;
+            int r = p.Length - 1;
+            while (l < r)
+            {
+                int m = (l + r) / 2;
+                if (Compare(p[m], s, len) < 0)
+                {
+                    l = m + 1;
+                }
+                else
+                {
+                    r = m;
+                }
+            }
+            if (Compare(p[l], s, len) == 0)
+            {
+                return l;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        private int Compare(String a, String b, int prefix)
+        {
+            for (int i = 0; i < prefix; i++)
+            {
+                if (a.Length == i)
+                {
+                    return -1;
+                }
+                if (a[i] != b[i])
+                {
+                    return a[i] - b[i];
+                }
+            }
+            return 0;
+        }
+
+        public IList<IList<String>> SuggestedProductsBinarySearchFromLibrary(String[] products, String searchWord)
+        {
+            IList<IList<String>> ans = new List<IList<String>>();
+            products = products.OrderBy(x => x).ToArray();
+            for (int i = 1; i <= searchWord.Length; ++i)
+            {
+                String cur = searchWord.Substring(0, i);
+                int k = Array.BinarySearch(products, cur);
+                while (k > 0 && cur.Equals(products[k - 1])) // in case there are more than 1 cur in products.
+                    --k; // find the first one. 
+                if (k < 0) // no cur in products. 
+                    k = ~k; // find the first one larger than cur.
+                List<String> suggestion = new List<String>();
+                for (int j = k + 3; k < products.Length && k < j && products[k].StartsWith(cur); ++k)
+                    suggestion.Add(products[k]);
+                ans.Add(suggestion);
+            }
+            return ans;
+        }
+
+        public IList<IList<string>> SuggestedProductsWithTrieStructure(string[] products, string searchWord)
+        {
+
+            // Sort products to store product names in lexicographic order
+            Array.Sort(products);
+
+            // Load product names into Trie, each TrieNode stores at most 3 suggested word
+            TrieNode root = new TrieNode();
+            foreach (string prod in products)
+            {
+                TrieNode curr = root;
+                foreach (char c in prod)
+                {
+                    if (curr.Children[c - 'a'] == null)
+                        curr.Children[c - 'a'] = new TrieNode();
+                    curr = curr.Children[c - 'a'];
+                    if (curr.Suggestion.Count < 3)
+                        curr.Suggestion.Add(prod);
+                }
+            }
+
+            // Build result based on searchWord
+            List<IList<string>> res = new List<IList<string>>();
+            TrieNode curr2 = root;
+            foreach (char c in searchWord)
+            {
+                if (curr2 != null)
+                    curr2 = curr2.Children[c - 'a'];
+
+                res.Add(curr2 == null ? new List<string>() : curr2.Suggestion);
+            }
+
+            return res;
+        }
+
+        public class TrieNode
+        {
+            public List<string> Suggestion;
+            public TrieNode[] Children;
+            public TrieNode()
+            {
+                Suggestion = new List<string>();
+                Children = new TrieNode[26];
+            }
+        }
+    }
+}
---
+++ LeetCodeChallenges/AMZ/05FiveNumberOfIslands.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeChallenges.AMZ
+{
+    /*
+     https://leetcode.com/problems/number-of-islands/
+     Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. 
+     An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. 
+     You may assume all four edges of the grid are all surrounded by water.
+            11110
+            11010
+            11000
+            00000
+            == 1 Island
+    */
+    public class FiveNumberOfIslands
+    {
+        /*
+         The algorithm works as follow:
+            Scan each cell in the grid.
+            If the cell value is '1' explore that island (go into all the directions <-  ^  \/  ->).
+                Mark the explored island cells with 'x'.
+                Once finished exploring that island, increment islands counter.
+                The arrays dx[], dy[] store the possible moves from the current cell. 
+                Two land cells ['1'] are considered from the same island if they are horizontally or vertically adjacent (possible moves (-1,0),(0,1),(0,-1),(1,0)). 
+                Two '1' diagonally adjacent are not considered from the same island.
+             */
+        private readonly int[] dx = { -1, 0, 0, 1 };
+        private readonly int[] dy = { 0, 1, -1, 0 };
+        private int NumIslands(char[][] grid)
+        {
+            if (grid == null || grid.Length == 0) return 0;
+            int islands = 0;
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    if (grid[i][j] == '1')
+                    {
+                        Explore(grid, i, j);
+                        islands++;
+                    }
+                }
+            }
+            return islands;
+        }           
+        /*
+            x1110
+            x1010
+            x1000
+            00000
+                */
+        private void Explore(char[][] grid, int i, int j)
+        {
+            grid[i][j] = 'x';
+            for (int d = 0; d < dx.Length; d++)
+            {
+                if (i + dy[d] < grid.Length && 
+                    i + dy[d] >= 0 && 
+                    j + dx[d] < grid[0].Length && 
+                    j + dx[d] >= 0 && 
+                    grid[i + dy[d]][j + dx[d]] == '1')
+                {
+                    Explore(grid, i + dy[d], j + dx[d]);
+                }
+            }
+        }
+
+        public void Main() {
+            /*
+            11110
+            11010
+            11000
+            00000
+                */
+            var grid = new char[4][];
+            grid[0] = new char[5] { '1', '1', '1', '1', '0' };
+            grid[1] = new char[5] { '1', '1', '0', '1', '0' };
+            grid[2] = new char[5] { '1', '1', '0', '0', '0' };
+            grid[3] = new char[5] { '0', '0', '0', '0', '0' };
+            NumIslands(grid);
+        }
+    }
+}
