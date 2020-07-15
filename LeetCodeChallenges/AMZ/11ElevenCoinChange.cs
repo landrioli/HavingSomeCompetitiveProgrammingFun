db484edbcd2457254c192f61b1928aca7d6f875d
EX100019 <landrioli@eservicios.indracompany.com>
2020-07-15 15:34:47 +0100

    challneges

---
+++ LeetCodeChallenges/AMZ/11ElevenCoinChange.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeChallenges.AMZ
+{
+    public class ElevenCoinChange
+    {
+        public int NumberOfWaysToMakeCoinChange(int[] coins, int amount)
+        {
+            var ways = new int[amount + 1];
+            ways[0] = 1; //Base case for zero ammount there are one way that is no way
+            foreach (var coin in coins)
+            {
+                for (int j = 1; j < ways.Length; j++)
+                {
+                    if (coin <= j)
+                    {
+                        ways[j] += ways[j - coin];
+                    }
+                }
+            }
+            return ways[amount];
+        }
+
+        /*The idea of the algorithm is to build the solution of the problem from top to bottom. It applies the idea described above. 
+         * It use backtracking and cut the partial solutions in the recursive tree, which doesn't lead to a viable solution.
+         * Тhis happens when we try to make a change of a coin with a value greater than the amount SS. 
+         * To improve time complexity we should store the solutions of the already calculated subproblems in a table.*/
+        public int FewestCoinsToAmount(int[] coins, int amount)
+        {
+            var ways = new int[amount + 1];
+            for (int i = 0; i < ways.Length; i++)
+            {
+                ways[i] = int.MaxValue;
+            }
+            ways[0] = 0;
+
+            foreach (var coin in coins)
+            {
+                for (int i = 1; i < ways.Length; i++)
+                {
+                    if (coin <= i)
+                    {
+                        ways[i] = Math.Min(ways[i], 1 + ways[i - coin]);
+                    }
+                }
+            }
+
+            return ways[amount] >= amount + 1 ? -1 : ways[amount];
+        }
+
+        public void Main()
+        {
+            FewestCoinsToAmount(new int[] { 2 }, 3);
+            FewestCoinsToAmount(new int[] { 1, 2, 5 }, 11);
+        }
+    }
+}
---
+++ LeetCodeChallenges/AMZ/12EditDistance.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeChallenges.AMZ
+{
+    public class EditDistance
+    {
+        // j == x == represent the row number
+        // i == y == represent the column number
+        // new int[columns][rows]; 4 columns 2 rows == [4][2]
+        public int MinDistance(string word1, string word2)
+        {
+            var dp = new int[word2.Length + 1][];
+            for (int i = 0; i < word2.Length + 1; i++)
+                dp[i] = new int[word1.Length + 1];
+
+            // Fill first colum with base case
+            for (int i = 1; i < word2.Length + 1; i++)
+                dp[i][0] = dp[i - 1][0] + 1;
+
+            // Fill first row with base case
+            for (int i = 1; i < word1.Length + 1; i++)
+                dp[0][i] = dp[0][i - 1] + 1;
+
+            for (int i = 1; i < dp.Length; i++)
+            {
+                for (int j = 1; j < dp[0].Length; j++)
+                {
+                    if (word1[j - 1] == word2[i - 1]) // Base case, here if is the same letter, we get the last calculated value
+                    {
+                        dp[i][j] = dp[i - 1][j - 1];
+                        continue;
+                    }
+                    // if letter is different, get the last better choince plus one more operation
+                    dp[i][j] = 1 + Math.Min(Math.Min(dp[i - 1][j - 1], dp[i - 1][j]), dp[i][j - 1]);
+                }
+            }
+
+            return dp[word2.Length][word1.Length];
+        }
+
+        public void Main() {
+            MinDistance("host","ros");
+        }
+    }
+}
